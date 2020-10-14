81e281f6c4dce15bcf8ee8b6e34393cc175899fc
windcloak <xfirecloak@gmail.com>
2020-10-13 21:14:56 -0500

    Added Font Awesome imports

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import pages
 import Header from './shared/Header/Header';
 import Footer from './shared/Footer/Footer';
-
 import Home from './pages/Home/Home';
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+// import { faChevronCircleUp, faCode } from "@fortawesome/free-solid-svg-icons";
+// library.add(fab, faChevronCircleUp, faCode);
 
 function App() {
   return (
@@ -20,6 +25,9 @@ function App() {
       <Route path="/about" component={About} /> */}
       </Switch>
       <Footer />
+
+      {/* <FontAwesomeIcon icon={faChevronCircleUp} size="3x" /> */}
+
     </div>
     </Router>
   );
